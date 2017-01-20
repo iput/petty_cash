d65869e3bfcd27f422ba619cdae214af8e4197f7
ozora\Nindy A <nindyagustina89@gmail.com>
2017-01-20 15:54:30 +0700

    controller beranda user

---
+++ application/controllers/S_beranda.php
@@ -0,0 +1,27 @@
+<?php 
+defined('BASEPATH')OR exit('No direct script access allowed');
+
+/**
+* 
+*/
+class S_beranda extends CI_Controller
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		if ($this->session->userdata('username')&& $this->session->userdata('idUser')){
+		$this->load->view('attribute/header_user');
+		$this->load->view('user/u_beranda');
+		$this->load->view('attribute/footer_user');
+		}
+		else{
+			$this->load->view('v_login');
+		}
+	}
+}
+ ?>
